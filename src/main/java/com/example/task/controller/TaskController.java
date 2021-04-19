273feb4635da0258761bab27a1f182a778f1c321
Alec Ahlbrandt <alec@ahlbrandt.net>
2021-04-19 18:32:35 -0500

    add task service and imp cont

--- src/main/java/com/example/task/controller/TaskController.java
+++ src/main/java/com/example/task/controller/TaskController.java
@@ -1,17 +1,22 @@
 package com.example.task.controller;
 
 import com.example.task.dto.Task;
+import com.example.task.service.TaskService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/task")
 public class TaskController implements ITaskController {
+    private final TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
     @Override
-    public void create(Task user) {
-
+    public void create(Task task) {
+        this.taskService.createTask(task);
     }
 
     @Override
-    public void delete(Task user) {
-
+    public void delete(Task task) {
+        this.taskService.deleteTask(task.getId());
     }
 }
--- src/main/java/com/example/task/service/TaskService.java
+++ src/main/java/com/example/task/service/TaskService.java
@@ -1,8 +1,22 @@
 package com.example.task.service;
 
+import com.example.task.dto.Task;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class TaskService {
+    private final Map<String, Task> stringTaskMap;
+    TaskService() {
+        this.stringTaskMap = new HashMap<>();
+    }
+    public void createTask(Task task) {
+        this.stringTaskMap.put(task.getId(), task);
+    }
 
+    public void deleteTask(String taskId) {
+        this.stringTaskMap.remove(taskId);
+    }
 }
