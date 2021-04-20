cc3f249ed80952b6fda98bc1674bd6a0fbc6c058
Alec Ahlbrandt <alec@ahlbrandt.net>
2021-04-19 20:45:43 -0500

    make user model and add hibernate

---
+++ src/main/java/com/example/task/model/User.java
@@ -0,0 +1,25 @@
+package com.example.task.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+}
